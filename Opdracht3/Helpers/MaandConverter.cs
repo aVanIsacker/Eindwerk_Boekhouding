5aec6b40c9025831bc99003129e7ce9fa851182b
AVIgyra <vanisackerandrea@yahoo.com>
2020-12-22 19:16:46 +0100

    date formatting

---
+++ Opdracht3/Helpers/MaandConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Opdracht3.Helpers
+{
+    public class MaandConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return ((DateTime)value).ToString("MMM").ToUpper();
+
+            return "";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return DateTime.Parse(value.ToString());
+            else
+                return DateTime.Today;
+        }
+    }
+}
