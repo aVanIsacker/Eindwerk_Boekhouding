fad771bc8baee2b2c74cf431bbcd285a04d69008
AVIgyra <vanisackerandrea@yahoo.com>
2020-12-26 20:15:03 +0100

    Betaaldatum enkel zichtbaar bij selectie betaald in combobox

---
+++ Opdracht3/Helpers/IntToVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Opdracht3.Helpers
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+       
+        //public object Convert(object value, Type targetType, object parameter, string language)
+        //{
+        //    int index = (int)value;
+        //    return index == 0 ? Visibility.Hidden : Visibility.Visible;
+        //}
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int index = (int)value;
+            return index == 2 ? Visibility.Hidden : Visibility.Visible;
+        }
+
+
+        //public object ConvertBack(object value, Type targetType, object parameter, string language)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
