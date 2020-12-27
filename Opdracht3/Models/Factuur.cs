3b69ad5a5d0357fee23056a2deb24a3f9cd4ec96
Louisa <louisa.vuylsteke@gmail.com>
2020-12-27 22:51:24 +0100

    utils toegevoegd om btw naar boven af te ronden

--- Opdracht3/Models/Factuur.cs
+++ Opdracht3/Models/Factuur.cs
@@ -65,7 +65,7 @@ namespace Opdracht3.ViewModels
         public DateTime BetaalDatum { get; set; }
        
 
-        public double BTWBedrag { get { return BedragExclBTW * BTWTarief / 100.0; }  }
+        public double BTWBedrag { get { return Utils.RoundUp(BedragExclBTW * BTWTarief / 100.0,2); }  }
 
         public DateTime VervalDatum { get; }
         //public DateTime VervalDatum { get { return _factuurDatum.AddMonths(1); } }
---
+++ Opdracht3/Utilities/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht3.Utilities
+{
+    /// <summary>
+    /// Utility class
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Geeft het getal terug afgerond naar boven met X aantal plaatsen na de comma.
+        /// </summary>
+        /// <param name="input">Input</param>
+        /// <param name="places">Aantal plaatsen na de comma</param>
+        /// <returns>Double afgerond</returns>
+        public static double RoundUp(double input, int places)
+        {
+            double multiplier = Math.Pow(10, Convert.ToDouble(places));
+            return Math.Ceiling(input * multiplier) / multiplier;
+        }
+    }
+}
