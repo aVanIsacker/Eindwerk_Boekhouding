49f177946b601a3e12724121784e5b82f96d64e7
Briana Manguette <brianamanguette@icloud.com>
2020-12-31 12:02:24 +0100

    klein detail vergetern bij de converter

--- Opdracht3/Helpers/PropertyValueStringConverter.cs
+++ Opdracht3/Helpers/PropertyValueStringConverter.cs
@@ -11,7 +11,10 @@ namespace Opdracht3.Helpers
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return value.ToString();
+            if (value != null)
+                return value.ToString();
+
+            return "";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
